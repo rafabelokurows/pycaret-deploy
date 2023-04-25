84806037d4ac376225e580d6a1380447d8316590
Rafael Belokurows <rafabelokurows@gmail.com>
2023-04-25 12:46:26 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,18 @@
+#mkdir -p ~/.streamlit/
+#echo "\
+#[general]\n\
+#email = \"rafabelokurows@gmail.com\"\n\
+#" > ~/.streamlit/credentials.toml
+#echo "\
+#[server]\n\
+#headless = true\n\
+#enableCORS=false\n\
+# port = $PORT\n\
+# " > ~/.streamlit/config.toml
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"rafabelokurows@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
+echo "[general]"  > ~/.streamlit/credentials.toml
+echo "email = \"rafabelokurows@gmail.com\""  >> ~/.streamlit/credentials.toml
+echo "[server]"  > ~/.streamlit/config.toml 
+echo "headless = true"  >> ~/.streamlit/config.toml
+echo "port = $PORT"  >> ~/.streamlit/config.toml
+echo "enableCORS = false"  >> ~/.streamlit/config.toml
